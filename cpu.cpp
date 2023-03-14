7a388d2454aa92cd06b450f8d42c68e8416574b5
silent <zesilentbeast2020@gmail.com>
2023-03-14 16:17:21 +0600

    Code can now read file + added missing include

--- cpu.cpp
+++ cpu.cpp
@@ -2,10 +2,27 @@
 #include <vector>
 #include <inttypes.h>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int32_t main(int32_t argc, char** argv)
 {
+    if (argc != 2)
+    {
+        cout << "Error: No file provided" << endl; 
+        return 1;
+    }
+
+    ifstream File(argv[1]);
+    if (!File.is_open())
+    {
+        cout << "Error: File not found" << endl;
+    }
+
+    string fileContents, line;
+    while(getline(File,line))
+        fileContents += line;
+    File.close();
     return 0;
 }
