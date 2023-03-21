fa8ab3f8ba0a9b1a94351e716a673ce84c76e3c3
silent <zesilentbeast2020@gmail.com>
2023-03-21 13:49:08 +0600

    added instruction seperator

--- assembler.py
+++ assembler.py
@@ -1,7 +1,42 @@
 import sys
 
 def main():
-    pass
+    if len(sys.argv) != 2:
+        print("Error: no file provided")
+        return 1
+
+    with open(sys.argv[1],"r") as file:
+        if not file.readable:
+            print("Error: file not found")
+            return 1
+        rawCode = file.read()
+    
+    rawLines = rawCode.split("\n")
+    instructions = []
+    for line in rawLines:
+        lastInstruction = ""
+        instruction = []
+
+        for char in line:
+            if char == " ":
+                if lastInstruction:
+                    instruction.append(lastInstruction) 
+                    lastInstruction = ""
+            elif char in ",:":
+                if lastInstruction:
+                    instruction.append(lastInstruction)
+                    lastInstruction = ""
+                instruction.append(char)
+            else:
+                lastInstruction += char
+        
+        if lastInstruction:
+            instruction.append(lastInstruction)
+        instructions.append(instruction)
+
+    print(instructions)
+
+
 
 if __name__ == "__main__":
     main()
