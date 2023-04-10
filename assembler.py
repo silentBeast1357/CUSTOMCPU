342a297ee47de1b63c056c243b643f4e45eca701
silent <zesilentbeast2020@gmail.com>
2023-04-10 08:58:04 +0600

    added jg and jge instruction

--- assembler.py
+++ assembler.py
@@ -168,6 +168,20 @@ def main():
                 print("invalid command. \',\' not present")
                 return 1
             output += itoh(cins,True) + "\n"
+        elif instruction[0] == "jg":
+            cins = htoi("3500000000000000")
+            cins += registers[instruction[1]]*16 + registers[instruction[3]]
+            if instruction[2] != ",":
+                print("invalid command. \',\' not present")
+                return 1
+            output += itoh(cins,True) + "\n"
+        elif instruction[0] == "jge":
+            cins = htoi("3600000000000000")
+            cins += registers[instruction[1]]*16 + registers[instruction[3]]
+            if instruction[2] != ",":
+                print("invalid command. \',\' not present")
+                return 1
+            output += itoh(cins,True) + "\n"
         elif instruction[0] == "call":
             output += "6000000000000001\n"
         elif instruction[0] == "ret":
